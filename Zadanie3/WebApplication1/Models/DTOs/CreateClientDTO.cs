8798b2cf6ec927111675a1977069a22245164120
s31029 <s31029@pjwstk.edu.pl>
2025-04-28 22:18:21 +0200

    Zadanie 3

---
+++ Zadanie3/WebApplication1/Models/DTOs/CreateClientDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models.DTOs
+{
+    public class CreateClientDTO
+    {
+        [Required] public string FirstName { get; set; }
+        [Required] public string LastName  { get; set; }
+        [Required][EmailAddress] public string Email     { get; set; }
+        [Required][Phone]        public string Telephone { get; set; }
+        [Required][RegularExpression(@"^\d{11}$")] public string Pesel { get; set; }
+    }
+}
