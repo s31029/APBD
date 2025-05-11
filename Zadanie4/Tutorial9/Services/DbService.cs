2aac5322f46c84fbf33e3b744c7556ebfe1dc6fc
s31029 <s31029@pjwstk.edu.pl>
2025-05-11 13:01:58 +0200

    Zadanie 4

---
+++ Zadanie4/Tutorial9/Services/DbService.cs
@@ -0,0 +1,68 @@
+﻿using System.Data;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+
+namespace Tutorial9.Services;
+
+public class DbService : IDbService
+{
+
+    private readonly IConfiguration _configuration;
+    public DbService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public async Task DoSomethingAsync()
+    {
+        await using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        await using SqlCommand command = new SqlCommand();
+        
+        command.Connection = connection;
+        await connection.OpenAsync();
+        
+        DbTransaction transaction = await connection.BeginTransactionAsync();
+        command.Transaction = transaction as SqlTransaction;
+
+        // BEGIN TRANSACTION
+        try
+        {
+            command.CommandText = @"INSERT INTO Animal VALUES(@IdAnimal, @NameAnimal)";
+            command.Parameters.AddWithValue("@IdAnimal", 1);
+            command.Parameters.AddWithValue("@NameAnimal", "Name");
+
+            await command.ExecuteNonQueryAsync();
+
+            command.Parameters.Clear();
+            command.CommandText = @"INSERT INTO Animal VALUES(@IdAnimal, @NameAnimal)";
+            command.Parameters.AddWithValue("@IdAnimal", 1);
+            command.Parameters.AddWithValue("@NameAnimal", "Name");
+
+            await command.ExecuteNonQueryAsync();
+            
+            await transaction.CommitAsync();
+        }
+        catch (Exception e)
+        {
+            await transaction.RollbackAsync();
+            throw;
+        }
+        // END TRANSACTION
+    }
+
+    public async Task ProcedureAsync()
+    {
+        await using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        await using SqlCommand command = new SqlCommand();
+        
+        command.Connection = connection;
+        await connection.OpenAsync();
+
+        command.CommandText = "AddProductToWarehouse";
+        command.CommandType = CommandType.StoredProcedure;
+        
+        command.Parameters.AddWithValue("@IdProduct", 1);
+        
+        await command.ExecuteScalarAsync();
+    }
+}
