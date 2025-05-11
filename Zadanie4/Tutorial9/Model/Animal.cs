5686dd2254471a6301e2ded345da0cbb2f2d9d82
s31029 <s31029@pjwstk.edu.pl>
2025-05-11 12:38:57 +0200

    Zadanie 4

---
+++ Zadanie4/Tutorial9/Model/Animal.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tutorial9.Model;
+
+public class Animal
+{
+    public int IdAnimal { get; set; }
+    [MaxLength(200)]
+    public string Name { get; set; }
+    public int Amount { get; set; }
+}
