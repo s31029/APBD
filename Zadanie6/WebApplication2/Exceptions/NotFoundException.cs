e6a13fcea1b4795052607a7d9229c2f0da1c7bc3
s31029 <s31029@pjwstk.edu.pl>
2025-06-12 12:13:48 +0200

    Zadanie 6

---
+++ Zadanie6/WebApplication2/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
