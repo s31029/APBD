4cc49012f925173258c1ccaa25e9f1db36aa63be
s31029 <s31029@pjwstk.edu.pl>
2025-06-12 12:06:17 +0200

    Delete Zadanie6/WebApplication2 directory

--- Zadanie6/WebApplication2/WebApplication2/Exceptions/NotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication2.Exceptions
-{
-    public class NotFoundException : Exception
-    {
-        public NotFoundException(string message) : base(message)
-        {
-        }
-    }
-}
